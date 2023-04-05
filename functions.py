64889dbabf4f638b3542b172c92c855e0eb65ebc
kalbar87 <mkalbarczyk87@gmail.com>
2023-04-05 13:55:57 +0200

    Fourth commit

--- functions.py
+++ functions.py
@@ -9,8 +9,6 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy import stats
-from sklearn.model_selection import  cross_val_score, train_test_split
-from sklearn.metrics import r2_score, mean_absolute_error
 
 
 
@@ -47,49 +45,3 @@ def distribution_plot(feature):
     axd['right'].boxplot(feature, medianprops=medianprops, boxprops=boxprops, capprops=capprops, whiskerprops=whiskerprops)
     axd['right'].set_xlabel(feature.name)
 
-def residual_plot(y_test, y_pred, fig, ax0, ax1, color, lbl,i):
-    res = y_pred-y_test
-    max_res = max(abs(res))
-    ind = np.where(abs(res) == max_res)
- 
-
-    ax0.plot(y_pred, res,'o', markeredgecolor=color, label=lbl, zorder=i)
-    ax0.axhline(y=0, xmin=0, xmax=40000, c='black', zorder=1)
-    ax0.vlines(x = y_pred[ind], ymin = 0, ymax = res.iloc[ind].values, color='dark%s' %color)
-    ax0.set_ylim(-30000,30000)
-    #ax0.set_xlim(-1,8)
-    ax0.set_title('Target Residual plot', fontsize=22)
-    ax0.set_ylabel('Residuals')
-    ax0.set_xlabel('Predictied values')
-    ax0.legend()
-    sns.histplot(y=res, bins=40,ax=ax1, color='light%s'%color, stat='density')
-    sns.kdeplot(y=res, color=color, ax=ax1)
-    ax1.axhline(y=0, xmin=0, xmax=5, c='black', zorder=1)
-    ax1.set_ylim(-30000, 30000)
-    ax1.text(0.3,1-0.05*i, '%s_mean = %.2f' %(lbl,np.mean(res)), transform=ax1.transAxes, fontsize=13)
-    ax1.axis('off')
-
-def qq_plot(real, pred, color, order, lbl, ax0, ax1):
-    plt.rc('lines', markersize='8', markeredgecolor=color, markerfacecolor='white', markeredgewidth=1, linewidth=2)
-    fig = stats.probplot(real-pred, plot=ax0) 
-    ax1.plot(real, pred, 'o', alpha=0.7, zorder=order, label=lbl)
-    sns.regplot(x=real, y=pred, scatter=False, ax=ax1,color='blue', ci=0)
-    ax1.set_xlabel('Charge')
-    ax1.set_ylabel('Predicted Charge')
-    ax1.set_title('Test vs predicted values')
-    ax1.legend()
-
-def model_score(model, X,y, kf):
-    X_train, X_test, y_train, y_test = train_test_split(X,y, random_state=42, test_size=0.2)
-    model.fit(X_train,y_train)
-    y_pred = model.predict(X_test)
-    RMSE = np.sqrt(-cross_val_score(model, X_train,y_train, cv=kf, scoring = 'neg_mean_squared_error')).mean()
-    MAE = mean_absolute_error(y_test, y_pred)
-    r2 = r2_score(y_test, y_pred)
-    _, p_value = stats.pearsonr(y_test, y_pred)
-    print('%s model\n\n' %str(model).split('(')[0],
-          'ROOT MEAN SQUARED ERROR:\t%.2f\n' %RMSE,
-          'MEAN ABSOLUTE ERROR:\t\t%.2f\n' %MAE,
-          'P-VALUE:\t\t\t%.1e\n\n' %p_value,
-          'R2 SCORE:\t\t\t%.2f %%' %(r2*100)
-          )
